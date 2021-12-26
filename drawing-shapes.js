01051248c4ef687b4ab0feac6ccd42846d4ffe38
Samuel Ânderson Gebing <samu.gebing@gmail.com>
2021-12-25 22:50:09 -0300

    gabonese-flag-lessons
    Here I mixed a few lessons which used the Gabonese Flag.

--- drawing-shapes.js
+++ drawing-shapes.js
@@ -1,5 +1,13 @@
 /*
-* First lesson
+* Comments
+*/
+// drawBox() --> function that
+// tells to the computer to draw
+// a box (of the color inside the
+// parenthesis, the argument)
+
+/*
+* First lesson - French Flag
 */
 
 // introduction
@@ -12,3 +20,55 @@ newLine();
 drawBox(blue);
 drawBox(white);
 drawBox(red);
+// I added (like I will do on the nexts) 
+// this line for a good execution 
+newLine();
+
+/*
+* Second lesson - Gabonese Flag
+*/
+
+// GABONESE FLAG
+// all line green
+// all line yellow 
+// all line blue
+
+// introduction 
+drawBox(green);
+drawBox(green);
+drawBox(green);
+newLine();
+drawBox(yellow);
+
+// resolution
+drawBox(yellow);
+drawBox(yellow);
+newLine();
+drawBox(blue);
+drawBox(blue);
+drawBox(blue);
+newLine();
+
+/* 
+* Third lesson - Gabonese Flag Helper
+*/
+
+// introduction
+drawBoxes('ggg');
+newLine();
+drawBoxes('yyy');
+
+// resolution
+drawBoxes('bbb');
+newLine();
+
+/*
+* Optional lesson - Gabonese Flag Shortcuts
+*/
+
+// introduction
+// drawBoxes('ggg y');
+
+//resolution
+drawBoxes('ggg yyy bbb');
+new Line();
