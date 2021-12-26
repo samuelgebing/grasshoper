fd563b6c48454f6e7f2a681360bd53e1679341b7
Samuel Ânderson Gebing <samu.gebing@gmail.com>
2021-12-26 03:52:11 -0300

    Drawing shapes (#2)
    * french-flag-lesson
    
    Here I just copy and solved the first lesson.
    
    * gabonese-flag-lessons
    
    Here I mixed a few lessons which used the Gabonese Flag.
    
    * Updating an error
    
    * random-boxes
    
    Here I used the pickRandom() function to pick a random color, and the drawBox() function to draw a box with this color

--- drawing-shapes.js
+++ drawing-shapes.js
@@ -1,1 +1,85 @@
+/*
+* Comments
+*/
+// drawBox() --> function that
+// tells to the computer to draw
+// a box (of the color inside the
+// parenthesis, the argument)
 
+/*
+* First lesson - French Flag
+*/
+
+// introduction
+drawBox(blue);
+drawBox(white);
+drawBox(red);
+newLine();
+
+// resolution
+drawBox(blue);
+drawBox(white);
+drawBox(red);
+// I added (like I will do on the nexts) 
+// this line for a good execution 
+newLine();
+
+/*
+* Second lesson - Gabonese Flag
+*/
+
+// GABONESE FLAG
+// all line green
+// all line yellow 
+// all line blue
+
+// introduction 
+drawBox(green);
+drawBox(green);
+drawBox(green);
+newLine();
+drawBox(yellow);
+
+// resolution
+drawBox(yellow);
+drawBox(yellow);
+newLine();
+drawBox(blue);
+drawBox(blue);
+drawBox(blue);
+newLine();
+
+/* 
+* Third lesson - Gabonese Flag Helper
+*/
+
+// introduction
+drawBoxes('ggg');
+newLine();
+drawBoxes('yyy');
+
+// resolution
+drawBoxes('bbb');
+newLine();
+
+/*
+* Optional lesson - Gabonese Flag Shortcuts
+*/
+
+// introduction
+// drawBoxes('ggg y');
+
+//resolution
+drawBoxes('ggg yyy bbb');
+newLine();
+
+/*
+* Forth lesson - "A Dash of Random"
+*/
+
+// introduction
+drawBox(pickRandom(color));
+// drawBox();
+
+// resolution
+drawBox(pickRandom(color));
